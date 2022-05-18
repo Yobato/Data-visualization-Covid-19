6f59f55f100c291dffbf2bc61028c14767536f13
Yobato <satriyobagus30077@gmail.com>
2022-05-18 19:36:42 +0700

    Chart fixed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
+import "chart.js/auto";
 
 import styles from "./Chart.module.css";
 
@@ -12,36 +13,33 @@ const Chart = () => {
       setDailyData(await fetchDailyData());
     };
 
-    // console.log(dailyData);
-
     fetchAPI();
   });
 
-  // const lineChart =
-  //   dailyData.length === 0 ? (
-  //     <Line
-  //       data={{
-  //         labels: dailyData.map(({ date }) => date),
-  //         datasets: [
-  //           {
-  //             data: dailyData.map(({ confirmed }) => confirmed),
-  //             label: "Infected",
-  //             borderColor: "#3333ff",
-  //             fill: true,
-  //           },
-  //           {
-  //             data: dailyData.map(({ deaths }) => deaths),
-  //             label: "Deaths",
-  //             borderColor: "red",
-  //             backgroundColor: "rgba(255, 0, 0, 0.5)",
-  //             fill: true,
-  //           },
-  //         ],
-  //       }}
-  //     />
-  //   ) : null;
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
 
-  return <div>{/* {lineChart} */}</div>;
+  return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
